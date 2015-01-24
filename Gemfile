47b4bae1233ce3ea1efc2a79e2be3f935cc4d706
Philip Greenwald <philip@avidest.com>
2015-01-24 17:00:06 -0500

    Installed devise, devise views, and user registration

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'simple_form'
+
+gem 'devise', '~> 3.4.0'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tweets#new'
-
+  
+  root 'static_pages#index'
   resources :tweets
-
+ devise_for :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
